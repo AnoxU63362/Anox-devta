ee04336698f03e7fb0afec94c25133b3823de45d
AnoxU63362 <uganti63362@gmail.com>
2026-08-30 16:12:47 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+export const WORKERS_PER_SESSION = 2;
+export const PER_TARGET = 800;
+export const PER_PAGE = 75;
+export const BATCH_FLUSH = 1000;
+export const MAX_RUNTIME_MIN = 800;
+export const STICKY_PROXY = false;
+
+export const GQL_HASHES = {
+    followers: '37479f2b8209594dde7facb0d904896a',
+    following: 'd04edd2229b57d9a3754f00d82f6f342',
+};
